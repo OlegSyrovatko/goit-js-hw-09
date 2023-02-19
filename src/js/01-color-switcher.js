b36b3e2f1dcff3bc9ec24bde9cffd2a5718213b7
Oleg <sirov@ukr.net>
2023-02-20 00:20:43 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+}
 
+refs.start.addEventListener("click", startColor);
+refs.stop.addEventListener("click", stopColor);
+let intervalId = null;
+
+function startColor() {
+
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    refs.start.setAttribute('disabled', 'true');
+}
+
+function stopColor() {
+    clearInterval(intervalId);
+    refs.start.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

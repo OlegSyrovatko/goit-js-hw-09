9673e7070ccc1a92621a82292967fa08d1d8858c
Oleg <sirov@ukr.net>
2023-02-22 00:01:53 +0200

    task start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,24 @@
+import Notiflix from 'notiflix'; 
+
+const refs = {
+  form: document.querySelector(".form"),
+  delay: document.getElementsByName("delay"),
+  step: document.getElementsByName("step"),
+  amount: document.getElementsByName("amount"),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault(); 
+    console.log(refs.delay[0].value);
+
+  Notiflix.Notify.warning(refs.delay[0].value);
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {

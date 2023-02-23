53239f5e797e5c6c693645a59ab5cd7fccbd0fe6
Oleg <sirov@ukr.net>
2023-02-23 10:54:25 +0200

    summary

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,42 +9,37 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 
-
-const createPromise = (i, allDelay) => {
-  return new Promise((resolve, reject) => {
-
-    // setTimeout(() => {
-    const shouldResolve = Math.random() > 0.3;
-
-    if (shouldResolve) {
-      Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${allDelay}ms`);
-    } else {
-      Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${allDelay}ms`);
-    }
-    // }, step); 
-  });
-};
-
 function onFormSubmit(e) {
   e.preventDefault();
-  const step = refs.step[0].value;
-  const delay = refs.delay[0].value;
-  let allDelay = Number(delay);
+  const step = Number(refs.step[0].value);
+  let delay = Number(refs.delay[0].value);
 
-  setTimeout(() => {
-    for (let position = 1; position <= refs.amount[0].value; position++) {
+  for (let position = 1; position <= refs.amount[0].value; position++) {
 
-      setTimeout(() => {
-        allDelay += Number(step);
-        console.log(step);
-        createPromise(position, allDelay)
-          .then(({ position, delay }) => {
+    setTimeout(() => {
+      createPromise(position, delay)
+        .then(({ position, delay }) =>
+          { Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);  },
+      ).catch(({ position, delay }) => {
+         Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`); 
+    });
+    }, delay);
 
-          })
-          .catch(({ position, delay }) => {
+    delay += step;
 
-          });
-      }, step);
-    }
-  }, delay);
+  }
 }
+
+const createPromise = (position, delay) => {
+
+  return new Promise((resolve, reject) => {
+
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+
+};
